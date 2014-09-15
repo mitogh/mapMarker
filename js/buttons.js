2b800e02f0b90a38d41dd3c7fc373ee08a46b376
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-09-15 12:15:28 -0500

    Clean the JS on the buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -1,19 +1,32 @@
 (function() {
-    tinymce.PluginManager.add('mapmarker', function( editor, url ) {
-        editor.addButton( 'mapmarker', {
-            icon: 'icon mapmarker-button',
-            tooltip: 'New Map',
-            cmd: 'openMapMarkerCreator',
-        });
-        editor.addCommand('openMapMarkerCreator', function() {
-            editor.windowManager.open({
-                file: ajaxurl + '?action=open_mapmarker_creator',
-                width: 800, 
-                height: 600,
-                inline: 1
-            }, {
-                plugin_url: url 
+    tinymce.create('tinymce.plugins.mapmarker', {
+        init: function(editor, url){
+            editor.addButton( 'mapmarker', {
+                icon: 'icon mapmarker-button',
+                tooltip: 'New Map',
+                cmd: 'openMapMarkerCreator',
             });
-        });
+            editor.addCommand('openMapMarkerCreator', function() {
+                editor.windowManager.open({
+                    file: ajaxurl + '?action=open_mapmarker_creator',
+                    width: 800, 
+                    height: 600,
+                    close_previous  : 'yes', 
+                    inline          : 'yes', 
+                }, {
+                    plugin_url: url 
+                });
+            });
+        },
+        getInfo: function(){
+            return {
+                longname    : 'Create maps with markers', 
+                author      : 'Crisoforo Gaspar', 
+                authorurl   : 'www.crisoforo.com', 
+                infourl     : 'github.com/mitogh', 
+                version     : tinymce.majorVersion +'.'+ tinymce.minorVersion
+            };
+        }
     });
+    tinymce.PluginManager.add('mapmarker', tinymce.plugins.mapmarker);
 })();
