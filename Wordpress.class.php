75c3a1838f37d710ac174286dd523214170914a2
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-09-18 11:38:16 -0500

    Improve comment on Wordpress

--- Wordpress.class.php
+++ Wordpress.class.php
@@ -2,7 +2,7 @@
 
 class Wordpress{
     /**
-     * Array to call the method from the class as is required more info:
+     * Array to call the method from the class in the action as is required, more info:
      * http://codex.wordpress.org/Function_Reference/add_action#Using_with_a_Class
      */
     protected function callMethod( $method_name ){
