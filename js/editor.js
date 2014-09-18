52b926141e5f3cef362c3acd1eceec371936eb43
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-09-18 12:36:01 -0500

    Add Dynamic width and height

--- js/editor.js
+++ js/editor.js
@@ -6,5 +6,17 @@
                 event.preventDefault();
             }
         });
+
+        $("#width").on('focusout keyup', function(){
+            var actualWidth = $(this).val();
+            $("#mapcontainer").css("width", actualWidth); 
+            google.maps.event.trigger(map, 'resize');
+        });
+
+        $("#height").on('focusout keyup', function(){
+            var actualHeight = $(this).val();
+            $("#mapcontainer").css("height", actualHeight); 
+            google.maps.event.trigger(map, 'resize');
+        });
     });
 })(jQuery);
